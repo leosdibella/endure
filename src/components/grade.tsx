2cd34f83545ebbd56e0eb08242ca1a3434bf61db
Leonardo DiBella <leosdibella@gmail.com>
2018-12-21 11:58:36 -0500

    Forgot to save this in time for the last commit, shame on me!

--- src/components/grade.tsx
+++ src/components/grade.tsx
@@ -26,6 +26,7 @@ class Grade extends React.PureComponent<GradeUtilities.IProps, GradeUtilities.St
     public readonly state: GradeUtilities.State = new GradeUtilities.State(this.expandGradeFill.bind(this), this.getDuration(), this.onAnimationComplete.bind(this));
 
     public componentDidUpdate(previousProps: GradeUtilities.IProps): void {
+        // TODO: Fix THIS
         if (this.props.mode === GameUtilities.Mode.paused) {
             this.state.animator.togglePaused();
         } else if (this.props.mode === GameUtilities.Mode.inGame) {
